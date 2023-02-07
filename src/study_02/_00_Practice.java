16347288202e9d0ec9e50500cd9d501ddfe47cd5
mklol00 <dlwodn97@naver.com>
2023-02-07 20:50:30 +0900

    20230117

---
+++ src/study_02/_00_Practice.java
@@ -0,0 +1,15 @@
+package study_02;
+
+import java.util.Scanner;
+
+public class _00_Practice {
+    public static void main(String[] args) {
+
+        String name;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("이름을 입력하세요.");
+        name = scanner.next();
+        System.out.println("입력하신 이름은 " + name + "입니다.");
+
+    }
+}
--- src/study_02/_05_Practice.java
+++ src/study_02/_05_Practice.java
@@ -1,7 +1,23 @@
 package study_02;
 
+import java.util.Scanner;
+
 public class _05_Practice {
     public static void main(String[] args) {
+
+        int num;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("3의 배수인지 판단하고 싶은 숫자를 입력하세요.");
+        num = scanner.nextInt();
+        if(num % 3 == 0){
+            System.out.println("3의 배수입니다.");
+        }
+        else {
+            System.out.println("3의 배수가 아닙니다.");
+        }
+
+
 
     }
 }
